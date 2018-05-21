5b65cf10d46d273a996f7653a59d5ac2e6d4bde2
mike.mitchell.nyc@gmail.com <sunshine1>
2018-05-21 15:38:37 -0700

    updated counter file to not call collatz() twice

--- CollatzCounter.py
+++ CollatzCounter.py
@@ -1,28 +1,34 @@
-#rileywestonmiller@outlook.com
-#practical programming, third edition by Gries, Campbell & Montojo
-#expert python programming - second edition
+# rileywestonmiller@outlook.com
+# practical programming, third edition by Gries, Campbell & Montojo
+# expert python programming - second edition
 
-def isEven(num):
-    return num%2==0
-    
-def Collatz(num):
-    count=0
-    while num!=1:
-        count=1+count
-        if isEven(num):
-            num=num/2
+
+def is_even(num):
+    return num % 2 == 0
+
+
+def collatz(num):
+    count = 0
+    while num != 1:
+        count = 1 + count
+        if is_even(num):
+            num = num/2
         else:
-            num=num*3+1
+            num = num * 3 + 1
     return count
 
-def CollatzCount(numbersToRun):   
-    previousCount=0
-    counter=0
-    while counter < numbersToRun:
-        counter+=1
-        if Collatz(counter)>previousCount:
-            previousCount=Collatz(counter)
-            winningNumber=counter
-    print(previousCount,winningNumber)
 
-CollatzCount(35000)
+def collatz_count(numbers_to_run):
+    previous_count = 0
+    counter = 0
+    while counter < numbers_to_run:
+        counter += 1
+        counted = collatz(counter)
+        if counted > previous_count:
+            previous_count = counted
+            winning_number = counter
+            print(previous_count, winning_number)
+    print(previous_count, winning_number)
+
+
+collatz_count(35000)
