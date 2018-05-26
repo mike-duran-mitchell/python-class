7768cb3a57e89cb402df9709b90d589b14f841e3
Mike Duran-Mitchell <mike.mitchell.nyc@gmail.com>
2018-05-26 00:58:33 -0700

    got hands

--- BlackJack.py
+++ BlackJack.py
@@ -1,15 +1,40 @@
 #blackjack
+from random import shuffle
+
 
 #cards
-values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
-suits = ['diamonds', 'spades', 'hearts', 'clubs']
-
-deck = []
 
 
-for i in range(len(values)):
-    for j in range(len(suits)):
-        print('i', i, 'ai', values[i], 'j', j, 'suits', suits[j])
-        j += 1
-    i += 1
+
+def new_deck():
+    values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+    suits = ['diamonds', 'spades', 'hearts', 'clubs']
+    a_new_deck = []
+    for i in range(len(values)):
+        for j in range(len(suits)):
+            a_new_deck.append([values[i], suits[j]])
+            j += 1
+        i += 1
+    return a_new_deck
+
+
+def shuffle_deck(deck_to_shuffle):
+    for i in range(3):
+        shuffle(deck_to_shuffle)
+    return deck_to_shuffle
+
+
+deck = new_deck()
+
+shuffle_deck(deck)
+
+
+def deal_cards(deck_to_deal):
+    return deck_to_deal[-5:]
+
+
+hand = deal_cards(deck)
+
+
+print(hand)
 
