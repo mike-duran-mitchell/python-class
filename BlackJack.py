0093192e5ca40426d585941850d4ae010dac2685
Mike Duran-Mitchell <mike.mitchell.nyc@gmail.com>
2018-05-26 01:59:24 -0700

    working on getting user input

--- BlackJack.py
+++ BlackJack.py
@@ -1,9 +1,10 @@
-#blackjack
 from random import shuffle
 
 
-#cards
-
+def shuffle_deck(deck_to_shuffle, num):
+    for i in range(num):
+        shuffle(deck_to_shuffle)
+    return deck_to_shuffle
 
 
 def new_deck():
@@ -15,26 +16,58 @@ def new_deck():
             a_new_deck.append([values[i], suits[j]])
             j += 1
         i += 1
+    shuffle_deck(a_new_deck, 3)
     return a_new_deck
 
 
-def shuffle_deck(deck_to_shuffle):
-    for i in range(3):
-        shuffle(deck_to_shuffle)
-    return deck_to_shuffle
+def new_hand(deck_to_deal):
+    hand = []
+    for i in range(2):
+        card = deck_to_deal.pop()
+        hand.append(card)
+    return hand
 
 
-deck = new_deck()
-
-shuffle_deck(deck)
-
-
-def deal_cards(deck_to_deal):
-    return deck_to_deal[-5:]
-
-
-hand = deal_cards(deck)
+def keep_deck_random(deck_to_deal):
+    if len(deck_to_deal) < 30:
+        deck_to_add = new_deck()
+        deck_to_deal.extend(deck_to_add)
+        shuffle_deck(deck_to_deal, 3)
+    return deck_to_deal
 
 
-print(hand)
+def make_hands(num, starting_deck):
+    hands = []
+    if num > 1:
+        for i in range(num):
+            keep_deck_random(starting_deck)
+            hands.append(new_hand(starting_deck))
+        return hands
+    else:
+        return new_hand(starting_deck)
 
+
+def hit(deck_to_deal):
+    keep_deck_random(deck_to_deal)
+    return deck_to_deal.pop()
+
+
+# new_hands = make_hands(50, deck)
+
+# print(dealer_hand, 'arg', len(new_hands), len(deck))
+
+def get_card_values(cards):
+    print(cards[0], cards[1])
+
+while True:
+    user_number = input("Enter the number of hands you'd like to play against the house: ")
+    if user_number == "quit":
+        break
+    else:
+        try:
+            deck = new_deck()
+            new_hands = make_hands(int(user_number), deck)
+            dealer_hand = new_hand(deck)
+            print("the dealer's hand is: ", get_card_values(dealer_hand))
+        except ValueError:
+            print("Invalid integer '%s', try again" % (user_number,))
