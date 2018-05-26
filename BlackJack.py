b626b5abfc77b22e280b143adda884f1993c04de
Mike Duran-Mitchell <mike.mitchell.nyc@gmail.com>
2018-05-26 03:00:23 -0700

    need to get card values assigned in a way that is helpful

--- BlackJack.py
+++ BlackJack.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from operator import itemgetter
 
 
 def shuffle_deck(deck_to_shuffle, num):
@@ -21,11 +22,11 @@ def new_deck():
 
 
 def new_hand(deck_to_deal):
-    hand = []
+    fresh_hand = []
     for i in range(2):
         card = deck_to_deal.pop()
-        hand.append(card)
-    return hand
+        fresh_hand.append(card)
+    return fresh_hand
 
 
 def keep_deck_random(deck_to_deal):
@@ -47,17 +48,49 @@ def make_hands(num, starting_deck):
         return new_hand(starting_deck)
 
 
+def get_card_details(cards):
+    print("The player is holding", len(cards), "cards of value", get_hand_value(cards))
+    for i in range(len(cards)):
+        print(cards[i][0], ' of ', cards[i][1])
+
+
+def one_card_down(cards):
+    for i in range(len(cards)):
+        if i == 0:
+            print('Facedown Card')
+        else:
+            print(cards[i][0], ' of ', cards[i][1])
+
+
 def hit(deck_to_deal):
     keep_deck_random(deck_to_deal)
-    return deck_to_deal.pop()
+    return [deck_to_deal.pop()]
 
 
-# new_hands = make_hands(50, deck)
+def get_hand_value(cards):
 
-# print(dealer_hand, 'arg', len(new_hands), len(deck))
+   #  print(sorted(cards), key=itemgetter(0))
+    cv = 0
+    for i in range(len(cards)):
+        n = cards[i][0]
+        count = 0
+        if n == 'Ace':
+            count += 1
+            user_decision = int(input('Do you want your Ace to be worth 1 or 11?'))
+            while user_decision not in (1, 11):
+                user_decision = int(input('Do you want your Ace to be worth 1 or 11?'))
+            if user_decision == 1:
+                cv += 1
+            elif user_decision == 11:
+                cv += 11
+        elif n in ('King', 'Queen', 'Jack'):
+            cv += 10
+        else:
+            cv += n
+        if cv > 21 and count > 1:
+            print('You have an ace, homie')
+    return cv
 
-def get_card_values(cards):
-    print(cards[0], cards[1])
 
 while True:
     user_number = input("Enter the number of hands you'd like to play against the house: ")
@@ -66,8 +99,22 @@ while True:
     else:
         try:
             deck = new_deck()
-            new_hands = make_hands(int(user_number), deck)
+            player_hands = make_hands(int(user_number), deck)
             dealer_hand = new_hand(deck)
-            print("the dealer's hand is: ", get_card_values(dealer_hand))
+            one_card_down(dealer_hand)
+            for index in range(int(user_number)):
+                hand = player_hands[index]
+                get_card_details(hand)
+                while True:
+                    user_hit = input("Hit me? (y/n)")
+                    while user_hit not in ('n', 'no', 'y', 'yes', 'ys', 'ye'):
+                        user_hit = input("Hit me? (y/n)")
+                    if user_hit in("n", "no"):
+                        break
+                    elif user_hit in('y', 'yes', 'ys', 'ye'):
+                        hand.extend(hit(deck))
+                        get_card_details(hand)
+                    else:
+                        break
         except ValueError:
             print("Invalid integer '%s', try again" % (user_number,))
