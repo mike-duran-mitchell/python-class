097f9ee30e15e6f31a0206c03e21c7c8750defa2
Mike Duran-Mitchell <mike.mitchell.nyc@gmail.com>
2018-05-29 17:00:10 -0700

    doneskis

--- BlackJack.py
+++ BlackJack.py
@@ -1,5 +1,4 @@
 from random import shuffle
-from operator import itemgetter
 
 
 def shuffle_deck(deck_to_shuffle, num):
@@ -10,7 +9,7 @@ def shuffle_deck(deck_to_shuffle, num):
 
 def new_deck():
     values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
-    suits = ['diamonds', 'spades', 'hearts', 'clubs']
+    suits = ['Diamonds', 'Spades', 'Hearts', 'Clubs']
     a_new_deck = []
     for i in range(len(values)):
         for j in range(len(suits)):
@@ -48,19 +47,12 @@ def make_hands(num, starting_deck):
         return new_hand(starting_deck)
 
 
-def get_card_details(cards):
-    print("The player is holding", len(cards), "cards of value", get_hand_value(cards))
-    for i in range(len(cards)):
-        print(cards[i][0], ' of ', cards[i][1])
-    return get_hand_value(cards)
-
-
 def one_card_down(cards):
     for i in range(len(cards)):
-        if i == 0:
-            print('Facedown Card')
+        if i != 0:
+            ""
         else:
-            print(cards[i][0], ' of ', cards[i][1])
+            print('\n############################################################################################## \n The dealer is holding a facedown card and a', cards[i][0], 'of', cards[i][1], '\n############################################################################################## \n')
 
 
 def hit(deck_to_deal):
@@ -68,60 +60,123 @@ def hit(deck_to_deal):
     return [deck_to_deal.pop()]
 
 
-def get_hand_value(cards):
+def print_hand(hand):
+    hand_as_string = ""
+    for i in range(len(hand)):
+        hand_as_string = hand_as_string + str(hand[i][0]) + "of" + hand[i][1] + "\n"
+    return hand_as_string
+
+
+def get_hand_value(cards, user=True):
     cv = 0
-    has_ace = []
+    has_ace = False
+    hand_as_string = ""
     for i in range(len(cards)):
         n = cards[i][0]
-        if n == 'Ace':
-            user_decision = int(input('Do you want your Ace to be worth 1 or 11?'))
-            while user_decision not in (1, 11):
-                user_decision = int(input('Do you want your Ace to be worth 1 or 11?'))
-            if user_decision == 1:
-                cv += 1
-                has_ace.append(1)
-            elif user_decision == 11:
-                cv += 11
-                has_ace.append(11)
-        elif n in ('King', 'Queen', 'Jack'):
+        hand_as_string = hand_as_string + "\n" + str(n) + " of " + cards[i][1]
+        if n in ('King', 'Queen', 'Jack'):
             cv += 10
+        elif n == 'Ace':
+            cv += 1
+            has_ace = True
         else:
             cv += n
-        if cv > 21:
-            if 11 in has_ace:
-                print('value of ace has been reduced from 11 to 1 - be careful of bustin out there!')
-                cv -= 10
-            else:
-                break
+    if user and has_ace:
+        if cv + 10 == 21:
+            cv = 21
+        elif cv + 10 < 21:
+            user_ace_value = input("You've got an ace! Default value is 1. Would you like that to be worth 11? y/n")
+            if user_ace_value == 'y':
+                cv += 11
+    if not user and has_ace and cv + 10 < 21:
+        cv += 10
+    if user and cv == 21:
+        print("One of your hands hit 21!")
+    print_hand(cards)
+    if user:
+        print('################################# Your Cards ########################################')
+        print("You are holding", len(cards), "cards of value", cv, ":", hand_as_string)
+        print('################################# Your Cards ########################################\n' )
+    elif not user:
+        print('################################# Dealer Cards ########################################')
+        print("Dealer is holding", len(cards), "cards of value", cv, ":", hand_as_string)
+        print('################################# Dealer Cards ########################################\n')
     return cv
 
 
-while True:
-    user_number = input("Enter the number of hands you'd like to play against the house: ")
-    if user_number == "quit":
-        break
+def play_another_game():
+    user_plays_another = input("Do you want to play again? (y/n) : ")
+    if user_plays_another == "y":
+        start_game()
     else:
+        print("Hasta luego!")
+        exit()
+
+
+def start_game():
+    deck = new_deck()
+    dealer_hand = new_hand(deck)
+    one_card_down(dealer_hand)
+    hands_value = []
+    winning_hand_count = 0
+    losing_hand_count = 0
+    while True:
+        user_number = input("Enter the number of hands you'd like to play against the house or type 'quit': ")
+        if user_number == "quit":
+            print("Hasta luego")
+            exit()
         try:
-            deck = new_deck()
             player_hands = make_hands(int(user_number), deck)
-            dealer_hand = new_hand(deck)
-            one_card_down(dealer_hand)
             for index in range(int(user_number)):
-                hand = player_hands[index]
-                get_card_details(hand)
-                while True:
-                    user_hit = input("Hit me? (y/n)")
-                    while user_hit not in ('n', 'no', 'y', 'yes', 'ys', 'ye'):
+                hand = player_hands[index] if int(user_number) > 1 else player_hands
+                initial_cards_value = get_hand_value(hand)
+                print_hand(hand)
+                if initial_cards_value == 21:
+                    print("###############################################")
+                    print("Natural blackjack!")
+                    print("###############################################\n")
+                    hands_value.append(99)
+                else:
+                    while True:
                         user_hit = input("Hit me? (y/n)")
-                    if user_hit in("n", "no"):
-                        break
-                    elif user_hit in('y', 'yes', 'ys', 'ye'):
-                        hand.extend(hit(deck))
-                        check_value = get_card_details(hand)
-                        if check_value > 21:
-                            print('You busted, buster! Watch out for loose seals!')
+                        while user_hit not in ('n', 'y'):
+                            user_hit = input("Hit me? (y/n)")
+                        if user_hit in "n":
+                            hand_value = get_hand_value(hand)
+                            hands_value.append(hand_value)
                             break
+                        elif user_hit in 'y':
+                            hand.extend(hit(deck))
+                            hand_value = get_hand_value(hand)
+                            if hand_value > 21:
+                                print("###############################################")
+                                print("### you busted, bronco! Onto the next hand! ###")
+                                print("###############################################\n")
+                                hands_value.append(0)
+                                if int(user_number) == 1:
+                                    play_another_game()
+                                else:
+                                    break
+                            else:
+                                continue
+            dealer_hand_value = get_hand_value(dealer_hand, False)
+            while dealer_hand_value < 17:
+                print("Dealer takes a hit!")
+                dealer_hand.extend(hit(deck))
+                dealer_hand_value = get_hand_value(dealer_hand, False)
+            if dealer_hand_value > 21:
+                print('all hands win! dealer busts!')
+                winning_hand_count = len(hands_value)
+            else:
+                for i in range(len(hands_value)):
+                    if dealer_hand_value <= hands_value[i]:
+                        winning_hand_count += 1
                     else:
-                        break
+                        losing_hand_count += 1
+            print('You won', winning_hand_count, 'games, and lost', losing_hand_count, 'games!')
+            play_another_game()
         except ValueError:
             print("Invalid integer '%s', try again" % (user_number,))
+
+
+start_game()
